c524c020a027f2474975d7547242e3c9ece55556
fvalto <skualo_fv@msn.com>
2023-10-28 14:54:21 -0300

    Week 6 chamber fixes

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -5,9 +5,9 @@ const windSpeedInput = document.getElementById("wind-speed-value");
 function calculateWindChill() {
     const temperature = parseFloat(temperatureInput.value);
     const windSpeed = parseFloat(windSpeedInput.value);
-
+    
     let windchill;
-
+    
     if (temperature <= 50 && windSpeed > 3) {
         windchill = 35.74 + (0.6215 * temperature) - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temperature * Math.pow(windSpeed, 0.16);
         document.getElementById("wind-chill").textContent = windchill.toFixed(2);
@@ -19,3 +19,8 @@ function calculateWindChill() {
 // Event listeners to update user input
 temperatureInput.addEventListener("input", calculateWindChill);
 windSpeedInput.addEventListener("input", calculateWindChill);
+
+// Default values
+temperatureInput.value = 32;
+windSpeedInput.value = 19;
+calculateWindChill();
