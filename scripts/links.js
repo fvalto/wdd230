39e994b0946bd7d785506b36bb8c91d00780d5ba
fvalto <skualo_fv@msn.com>
2023-11-11 20:22:14 -0300

    Data driven menu

--- scripts/links.js
+++ scripts/links.js
@@ -1,10 +1,32 @@
 const baseURL = "https://fvalto.github.io/wdd230/";
 const linksURL = "https://fvalto.github.io/wdd230/data/links.json";
+const linkList = document.querySelector('#linkList');
 
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.table();
+    console.table(data.lessons);
+    
+    displayLinks(data.lessons);
+}
+
+function displayLinks(weeks) {
+    weeks.forEach(week => {
+        let li = document.createElement("li");
+
+        let weekNumber = document.createElement("b");
+        weekNumber.textContent = `Week ${week.lesson} `
+        li.appendChild(weekNumber);
+        
+        week.links.forEach(link => {
+            let a = document.createElement("a");
+            a.setAttribute("href", link.url);
+            a.textContent = link.title;
+            
+            li.append(" | ", a)
+        }); 
+        linkList.appendChild(li);
+    });
 }
 
 getLinks();
