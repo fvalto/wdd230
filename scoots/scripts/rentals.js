0cd4104c4ba0d9f47ece5309dfdbcf8b486e8737
fvalto <skualo_fv@msn.com>
2023-12-12 07:46:26 -0300

    advances

---
+++ scoots/scripts/rentals.js
@@ -0,0 +1,58 @@
+const url = 'https://fvalto.github.io/wdd230/scoots/data/rentals.json';
+const rentalsDiv = document.querySelector('.rentals-info');
+
+async function getRentalsData() {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+        displayRentals(data.rentals);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+function displayRentals(data) {
+    let table = document.createElement('table');
+    table.classList.add('rentals-table');
+
+    let headerRow = table.insertRow();
+    for (let key in data[0]) {
+        if (key !== 'reservation' && key !== 'walkIn') {
+            let th = document.createElement('th');
+            th.textContent = key.charAt(0).toUpperCase() + key.slice(1);
+            headerRow.appendChild(th);
+        }
+    }
+
+    // TABLE'S HEADERS
+    let reservationTh = document.createElement('th');
+    reservationTh.textContent = 'Reservation Pricing';
+    headerRow.appendChild(reservationTh);
+
+    let walkInTh = document.createElement('th');
+    walkInTh.textContent = 'Walk-In Pricing';
+    headerRow.appendChild(walkInTh);
+
+    // TABLE
+    data.forEach(function (rental) {
+        let row = table.insertRow();
+        for (let key in rental) {
+            if (key !== 'reservation' && key !== 'walkIn') {
+                let cell = row.insertCell();
+                cell.textContent = rental[key];
+            }
+        }
+
+        // CELLS
+        let reservationCell = row.insertCell();
+        reservationCell.textContent = `Half Day: ${rental.reservation.halfDay}, Full Day: ${rental.reservation.fullDay}`;
+
+        let walkInCell = row.insertCell();
+        walkInCell.textContent = `Half Day: ${rental.walkIn.halfDay}, Full Day: ${rental.walkIn.fullDay}`;
+    });
+
+    rentalsDiv.appendChild(table);
+}
+
+getRentalsData();
---
+++ scoots/scripts/weather.js
@@ -0,0 +1,54 @@
+const apiKey = '6d2ec2d7e5e10c69531c387af62fce4b';
+const lat = '20.5083';
+const lon = '-86.9458';
+const part = "";
+
+const currentURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`
+const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
+
+async function apiFetch() {
+    try {
+      const currentResponse = await fetch(currentURL);
+      const forecastResponse = await fetch(forecastURL);
+  
+      if (currentResponse.ok && forecastResponse.ok) {
+        const currentData = await currentResponse.json();
+        const forecastData = await forecastResponse.json();
+        
+        console.table(currentData);
+        console.table(forecastData);
+  
+        displayResults(currentData, forecastData);
+      } else {
+        throw Error(await response.text());
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+function displayResults(currentData, forecastData) {
+    const weatherFig = document.getElementById('weather-fig');
+    const captionDesc = document.querySelector('.iconcaption');
+    let desc = currentData.weather[0].description;
+    captionDesc.textContent = `${desc}`;
+    const iconsrc = `https://openweathermap.org/img/w/${currentData.weather[0].icon}.png`;
+    const weatherIcon = document.createElement('img');
+    weatherIcon.setAttribute('id', 'weather-icon')
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('loading', 'lazy');
+    weatherIcon.setAttribute('alt', 'weatherIcon');
+    weatherFig.append(weatherIcon);
+
+    const currentTemperature = currentData.main.temp;
+    document.getElementById('current-temperature').innerText = currentTemperature;
+  
+    const currentHumidity = currentData.main.humidity;
+    document.getElementById('current-humidity').innerText = currentHumidity;
+
+    const nextDayForecast = forecastData.list[0]
+    const nextDayTemperature = nextDayForecast.main.temp;
+    document.getElementById('next-day-forecast').innerText = nextDayTemperature;
+}
+
+apiFetch();
